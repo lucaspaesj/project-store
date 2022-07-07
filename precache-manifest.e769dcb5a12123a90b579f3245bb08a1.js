189efaf926a5ffff5b6f1f9e2da7507b06427e9d
lucaspaesj <lpjanuzi@gmail.com>
2022-07-07 14:28:35 -0300

    Updates

---
+++ precache-manifest.e769dcb5a12123a90b579f3245bb08a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89571fb296fda27f87c48d5322f34a27",
+    "url": "/project-store/index.html"
+  },
+  {
+    "revision": "761791f8a2c64fab1498",
+    "url": "/project-store/static/css/main.c9360f1f.chunk.css"
+  },
+  {
+    "revision": "3701c206af87fc36fe43",
+    "url": "/project-store/static/js/2.19544ba8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-store/static/js/2.19544ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761791f8a2c64fab1498",
+    "url": "/project-store/static/js/main.dc0f0617.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/project-store/static/js/main.dc0f0617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0fc282ca67b0eb91b6",
+    "url": "/project-store/static/js/runtime-main.ddb6afe9.js"
+  }
+]);
